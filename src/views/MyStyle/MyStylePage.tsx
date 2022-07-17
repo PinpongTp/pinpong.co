6275ec8a2235e5dec32a2c1f1d8a0402a0de6436
PinpongTp <pinpong.tp@gmail.com>
2022-07-17 21:32:03 +0700

    edit test home page and stype page

--- src/views/MyStyle/MyStylePage.tsx
+++ src/views/MyStyle/MyStylePage.tsx
@@ -1,41 +1,43 @@
 function MyStylePage() {
-    return (
-        <>
-            {/* <h3>My Style</h3> */}
-            <p>
-                ไม่มีฟ้อนต์ สไตล์น้อยๆ ลองดูซิว่ามันจะทำให้ชีวิต ยากขึ้นหรือง่ายลง ทั้งหมดของเว็บไซต์ใช้สไตล์หลักๆเท่านี้ อาจจะมีเพิ่มเติมในแต่ละส่วน แต่ก็จะพยายามใช้ให้น้อยที่สุด
-            </p>
-            <p>
-                html {'{'} <br />
-                &emsp;max-width: 70ch; <br />
-                &emsp;padding: 3em 1em; <br />
-                &emsp;margin: auto; <br />
-                &emsp;line-height: 1.75; <br />
-                &emsp;font-size: 1.25em; <br />
-                &emsp;color: #333; <br />
-                {'}'}
-            </p>
-            <p>
-                a {'{'} <br />
-                &emsp;color: chocolate; <br />
-                &emsp;text-decoration: none; <br />
-                {'{'}
-            </p>
-            <p>
-                <b>ใช่แล้ว, มันอาจจะดูน้อยมาก แต่มันก็คือภาพที่ทุกคนเห็นอยู่นี้แหละ</b>
-                <br />
-                <i>
-                    ref:{' '}
-                    <a
-                        href="https://www.swyx.io/css-100-bytes/"
-                        target="_blank"
-                    >
-                        https://www.swyx.io/css-100-bytes/
-                    </a>
-                </i>
-            </p>
-        </>
-    )
+  return (
+    <>
+      {/* <h3>My Style</h3> */}
+      <p>
+        ไม่มีฟ้อนต์ สไตล์น้อยๆ ลองดูซิว่ามันจะทำให้ชีวิต ยากขึ้นหรือง่ายลง ทั้งหมดของเว็บไซต์ใช้สไตล์หลักๆเท่านี้
+        อาจจะมีเพิ่มเติมในแต่ละส่วน แต่ก็จะพยายามใช้ให้น้อยที่สุด
+      </p>
+      <pre>
+        <code>
+          html {'{'} <br />
+          &emsp;&emsp;max-width: 70ch; <br />
+          &emsp;&emsp;padding: 3em 1em; <br />
+          &emsp;&emsp;margin: auto; <br />
+          &emsp;&emsp;line-height: 1.75; <br />
+          &emsp;&emsp;font-size: 1.25em; <br />
+          &emsp;&emsp;color: #333; <br />
+          {'}'}
+        </code>
+      </pre>
+      <pre>
+        <code>
+          a {'{'} <br />
+          &emsp;&emsp;color: chocolate; <br />
+          &emsp;&emsp;text-decoration: none; <br />
+          {'{'}
+        </code>
+      </pre>
+      <p>
+        <b>ใช่แล้ว, มันอาจจะดูน้อยมาก แต่มันก็คือภาพที่ทุกคนเห็นอยู่นี้แหละ</b>
+        <br />
+        <i>
+          ref:{' '}
+          <a href="https://www.swyx.io/css-100-bytes/" target="_blank">
+            https://www.swyx.io/css-100-bytes/
+          </a>
+        </i>
+      </p>
+    </>
+  );
 }
 
-export default MyStylePage
+export default MyStylePage;
