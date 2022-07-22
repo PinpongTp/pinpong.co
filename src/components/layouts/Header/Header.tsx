8756935d5311e3cf5f3f60943c37bf0e4fcd53ce
PinpongTp <pinpong.tp@gmail.com>
2022-07-22 19:13:38 +0700

    remove workflow

--- src/components/layouts/Header/Header.tsx
+++ src/components/layouts/Header/Header.tsx
@@ -1,18 +1,18 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faGithub, faFacebook } from '@fortawesome/free-brands-svg-icons'
+//import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+//import { faGithub, faFacebook } from '@fortawesome/free-brands-svg-icons'
 
 function Header() {
-    return (
-        <div className="d-flex justify-content-between align-items-end">
-            <div className=''>
-                <h1>Pinpong.co</h1>
-            </div>
-            {/* <div className="social">
+  return (
+    <div className="d-flex justify-content-between align-items-end">
+      <div className="">
+        <h1>Pinpong.co</h1>
+      </div>
+      {/* <div className="social">
                 <FontAwesomeIcon className='me-1' icon={faGithub} />
                 <FontAwesomeIcon icon={faFacebook} />
             </div> */}
-        </div>
-    )
+    </div>
+  );
 }
 
-export default Header
+export default Header;
