ac120d9b450bb738ab1a8d7dae39a84893993ef9
Pinpong Tongpat <pinpong.tp@gmail.com>
2023-02-09 16:43:19 +0700

    feat: add description

--- nuxt.config.js
+++ nuxt.config.js
@@ -11,7 +11,11 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
+      {
+        hid: 'description',
+        name: 'description',
+        content: `Welcome to Pinpong Tongpat's personal website, located at pinpong.co! As a talented web developer, I specialize in creating stunning and functional web applications. My expertise in front-end and back-end development allows me to provide comprehensive solutions for all your web development needs. Whether you're looking to build a responsive website, integrate a database, or bring your digital vision to life, I'm here to help. Check out my portfolio and see for yourself why I'm the web developer you've been looking for. Get in touch with me today at pinpong.co to discuss your next project.`,
+      },
       { name: 'format-detection', content: 'telephone=no' },
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
