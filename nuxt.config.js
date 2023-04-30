c4c4f6b45a878b679a7df778260bf15fd6644719
Pinpong Tongpat <pinpong.tp@gmail.com>
2023-04-30 23:49:22 +0700

    feat: update page title

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,7 +4,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'pinpong.co.4',
+    title: 'Pinpong.co',
     htmlAttrs: {
       lang: 'en',
     },
--- pages/note/_slug.vue
+++ pages/note/_slug.vue
@@ -6,11 +6,30 @@
 
 <script lang="ts">
 import Vue from 'vue'
+
+declare module 'vue/types/vue' {
+  interface Vue {
+    article: any
+  }
+}
+
 export default Vue.extend({
-  async asyncData({ $content, params }) {
+  async asyncData({ $content, params }): Promise<{ article: any }> {
     const article = await $content('note', params.slug).fetch()
     return {
       article,
+    }
+  },
+  head() {
+    return {
+      title: `Note: ${this.article.title} | Pinpong.co`,
+      meta: [
+        {
+          hid: 'description',
+          name: 'description',
+          content: this.article.description,
+        },
+      ],
     }
   },
 })
