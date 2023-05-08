747a63addba22066072f85add0a720d97632fae1
Pinpong Tongpat <pinpong.tp@gmail.com>
2023-05-08 23:26:07 +0700

    feat: update domain in ring link

--- components/Header.vue
+++ components/Header.vue
@@ -9,7 +9,7 @@
             </td>
             <td>
               <a
-                href="https://webring.wonderful.software#YOUR.DOMAIN"
+                href="https://webring.wonderful.software#pinpong.co"
                 title="วงแหวนเว็บ"
               >
                 <img
